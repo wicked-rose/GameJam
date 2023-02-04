cfeb613eae40cb33713627a229e4ad45c6a7d0e6
brandonDavis264 <73512985+brandonDavis264@users.noreply.github.com>
2023-02-03 23:55:00 -0500

    again

--- Assets/Scroll.cs
+++ Assets/Scroll.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Scroll : MonoBehaviour
 {
     public float speed = 0.5f;
+    float distance = 0f; 
+    public Material [] bg;
+    public float BackgroundChangeMultiplier = 1.5f;
     void Start()
     {
         
@@ -13,8 +17,16 @@ public class Scroll : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        distance += Time.deltaTime;
         //Vector2 offset = new Vector2(Time.time * speed, 0);
         Vector2 offset = new Vector2(0, - Time.time * speed);
         gameObject.GetComponent<Renderer>().material.mainTextureOffset = offset;
+        
+
+    }
+
+    void changedBackground(int index)
+    {
+        gameObject.GetComponent<Renderer>().material = bg[index];
     }
 }
