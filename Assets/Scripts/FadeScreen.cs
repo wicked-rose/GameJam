1ab49a1d231b859c3e81a11940156b98575f621c
Emma <bailly.emma@ufl.edu>
2023-02-04 18:40:47 -0500

    collectible script

--- Assets/Scripts/FadeScreen.cs
+++ Assets/Scripts/FadeScreen.cs
@@ -5,21 +5,37 @@ using UnityEngine.SceneManagement;
 
 public class FadeScreen : MonoBehaviour
 {
+    public GameObject fade;
     void Start()
     {
+        //fade = gameObject.GetComponent<Animation>();
         //Start the coroutine we define below named ExampleCoroutine.
         StartCoroutine(ExampleCoroutine());
     }
 
     IEnumerator ExampleCoroutine()
     {
-        //Print the time of when the function is first called.
-        Debug.Log("Started Coroutine at timestamp : " + Time.time);
+        // change background after 30 seconds
+        yield return new WaitForSeconds(10);
+        StartCoroutine(FadeInOut());
 
-        //yield on a new YieldInstruction that waits for 5 seconds.
-        yield return new WaitForSeconds(5);
-
-        //After we have waited 5 seconds print the time again.
-        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+        // change background after 30 seconds
+        yield return new WaitForSeconds(12);
+        StartCoroutine(FadeInOut());
     }
+
+    public IEnumerator FadeInOut() {
+        fade.SetActive(true);
+        fade.GetComponent<Animator>().SetBool("FadeOut", true);
+        yield return new WaitForSeconds(1);
+        fade.GetComponent<Animator>().SetBool("FadeOut", false);
+
+        fade.SetActive(true);
+        fade.GetComponent<Animator>().SetBool("FadeIn", true);
+        yield return new WaitForSeconds(1);
+        fade.GetComponent<Animator>().SetBool("FadeIn", false);
+        fade.SetActive(false);
+
+    }
+
 }
