4a50ca54a39d0d878bbd34249c9469c1258aaf76
brandonDavis264 <73512985+brandonDavis264@users.noreply.github.com>
2023-02-03 23:06:20 -0500

    moving back
    moving back

---
+++ Assets/Parallax.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    //Attached to gameObject (Background)
+    
+    //Effects the speed of background 
+    public float speed = 2;
+    void Start()
+    {
+       //Grab the edges of the background picture
+    }
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scroll.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll : MonoBehaviour
+{
+    public float speed = 0.5f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector2 offset = new Vector2(Time.time * speed, 0);
+        Vector2 offset = new Vector2(0, - Time.time * speed);
+        gameObject.GetComponent<Renderer>().material.mainTextureOffset = offset;
+    }
+}
