1ce0deb4b4ce27105dc1b25b11f0539b4236a7c4
Emma <bailly.emma@ufl.edu>
2023-02-05 05:25:14 -0500

    split root, start screen

--- Assets/Scripts/ObstacleItem.cs
+++ Assets/Scripts/ObstacleItem.cs
@@ -3,16 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ObstacleItem : MonoBehaviour
-{
+{ 
+    private Nutrients nutrientLevel;
+    public SpriteRenderer sprite;
+    public GameObject rootObj;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        nutrientLevel = FindObjectOfType<Nutrients>();
+        //
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            nutrientLevel.DecreaseHealth(10);
+            this.gameObject.SetActive(false);
+        }
     }
 }
