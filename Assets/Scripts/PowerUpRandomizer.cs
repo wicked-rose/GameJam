4e19ee756f116e43b5d08f5f2f6baa6cf7177841
camerongyoung <cameron.young@ufl.edu>
2023-02-04 20:18:13 -0500

    ItemSpawn Scene

--- Assets/Scripts/PowerUpRandomizer.cs
+++ Assets/Scripts/PowerUpRandomizer.cs
@@ -2,18 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GemRandomizer : MonoBehaviour
+public class PowerUpRandomizer : MonoBehaviour
 {
 
-    public GameObject gem;
+    public GameObject[] powerUps;
     private GameObject sprite;
     public bool stopSpawning = false;
     public float spawnTime;
     public float spawnDelay;
     public Transform target;
-    //private Vector3 xPos;
     private Vector3 cameraOffset;
-    
+
 
     // Start is called before the first frame update
     void Start()
@@ -24,14 +23,13 @@ public class GemRandomizer : MonoBehaviour
     {
         cameraOffset = new Vector3(Random.Range(-10.0f, 10.0f), -10.0f, 3.0f);
         target.transform.position = Camera.main.transform.position + cameraOffset;
-        sprite = Instantiate(gem, target.position, Quaternion.identity);
+        sprite = Instantiate(powerUps[Random.Range(0, powerUps.Length)], target.position, Quaternion.identity);
         StartCoroutine(selfDestruct());
     }
 
     IEnumerator selfDestruct()
     {
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(1f);
         Object.Destroy(sprite);
     }
-    
 }
