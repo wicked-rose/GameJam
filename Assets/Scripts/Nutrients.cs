e423d5fd67eaf3d4d149a215e6bae4128360e785
Emma <bailly.emma@ufl.edu>
2023-02-04 22:37:34 -0500

    update root stuff

---
+++ Assets/Scripts/Nutrients.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nutrients : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+    public GameObject HealthBar;
+    public bool useHealthBar = true;
+    [HideInInspector] public int index = 0; //for editor uses
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
