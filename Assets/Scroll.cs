c45f8450bd318004135ac22861b340ade23a29fe
brandonDavis264 <73512985+brandonDavis264@users.noreply.github.com>
2023-02-05 01:02:42 -0500

    Backgrounds

--- Assets/Scroll.cs
+++ Assets/Scroll.cs
@@ -1,38 +1,67 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor;
+using UnityEditorInternal;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UIElements;
 
 public class Scroll : MonoBehaviour
 {
     public float speed = 0.5f;
-    public float distance = 0f; 
-    public Material [] bg;
-    public float FirstChange = 5;
-    public float BackgroundChangeMultiplier = 1.5f;
-    void Start()
-    {
-        
-    }
+    public float distance = 0f;
+    public Texture Transition;
+    public float TransistionDuration = 3;
+    public Texture [] bg;
+    public float whenToTransition = .30f;
+    public float multiplier = 2f;
+
+    private float start = 0;
+    private float end = 1;
+
+    public bool Reverse = false;
+    public float Scale = 1f;
+
+    //private void Start()
+    //{
+
+    //}
 
     // Update is called once per frame
     void Update()
     {
         distance += Time.deltaTime;
-        Vector2 offset = new Vector2(0, - Time.time * speed);
-        gameObject.GetComponent<Renderer>().material.mainTextureOffset = offset;
+        float prevDist =30;
+        if (distance > 30 && distance < 50)
+        {
+            float ratio = (distance - 35) / (35 - prevDist);
+            gameObject.GetComponent<Renderer>().material.SetFloat("_FlipFlop", Mathf.SmoothStep(start, end, ratio));
+        }
 
+        if (distance > 50 && distance < 70)
+        {
+             gameObject.GetComponent<Renderer>().material.SetTexture("_FirstTexture", bg[1]);
+             gameObject.GetComponent<Renderer>().material.SetTexture("_SecondTexture", bg[2]);
+             gameObject.GetComponent<Renderer>().material.SetFloat("_FlipFlop", 0);
+             prevDist = distance;
+        }
+        if(distance > 70)
+        {
+            start = 0;
+            end = 1;
+            float ratio = (distance-75) / (75 - prevDist);
+            Debug.Log((TransistionDuration) / (distance- prevDist));
+            gameObject.GetComponent<Renderer>().material.SetFloat("_FlipFlop", Mathf.SmoothStep(start, end, ratio));
+        }
         
+        if (!Reverse)
+            gameObject.GetComponent<Renderer>().material.SetFloat("_Scale", .5f);
+        else
+            gameObject.GetComponent<Renderer>().material.SetFloat("_Scale", -4f);
 
-       // for(int i = 1; i <= bg.Length; i++)
-         //   if (distance > FirstChange && distance <= FirstChange*i*1.5 ) 
-           //     changedBackground(i-1);
- 
-    }
 
-    void changedBackground(int index)
-    {
-        //Resources.Load("Assets/scenes/");
-        gameObject.GetComponent<Renderer>().material = bg[index];
     }
+    
 }
